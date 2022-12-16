a5c8fe4225ab6ccb4142e57792bd2342864a4b8f
iMJ007 <mohammed.junaid1000@gmail.com>
2022-12-16 23:44:46 +0530

    Updated OG tags

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,8 @@ export default function Home() {
 				<meta name="twitter:card" content="summary"/>
 				<meta property="og:title" content="Junaid Gandhi | Android & Front End developer"/>
 				<meta property="og:description" content="I'm Junaid Gandhi, a Web and Android developer from Mumbai who loves speaking about HTML, CSS, JavaScript, web development, Java and Android along with CrossFit."/>
-				<meta property="og:image" content="https://profile.junaidgandhi.com/android-chrome-192x192.png"/>
-				<meta property="og:url" content="https://profile.junaidgandhi.com"/>
+				<meta property="og:image" content="https://junaidgandhi.com/android-chrome-192x192.png"/>
+				<meta property="og:url" content="https://junaidgandhi.com"/>
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
