3d73e59509f0c73eb6636b55d21b80daf2e2378c
iMJ007 <mohammed.junaid1000@gmail.com>
2022-12-23 17:47:59 +0530

    Orphic Priv Policy

---
+++ pages/orphic/policy/index.jsx
@@ -0,0 +1,117 @@
+function policy() {
+	return (
+		<div style={{ backgroundColor: "white" }}>
+			Privacy Policy
+			<br />
+			<br />
+			Welcome to Orphic ("Orphic," "we," "us" or "our"). Respect for your
+			privacy is coded into our environment. We&apos;ve built our Services with
+			a set of strong privacy principles in mind. - Our Privacy Policy explains
+			how we use, share and protect information in relation to our mobile
+			services and your choices about the collection and use of your
+			information.
+			<br />
+			<br />
+			Junaid Gandhi (Eclectik Devs) built the Orphic app as a free app. This
+			SERVICE is provided by Eclectik Devs at no cost and is intended for use as
+			is.
+			<br />
+			<br />
+			1. INFORMATION WE COLLECT
+			<br />
+			<br />
+			We collect the following types of information. Information you provide us
+			directly: Your Name, Google Profile Picture and e-mail address when you
+			register using Google Sign In. This information allows us to help to
+			provide you with a better user experience. WE DO NOT SHARE THIS
+			INFORMATION WITH ANYONE/ NOR THIS INFORMATION IS VISIBLE TO ANYONE EXCEPT
+			YOU. Analytics information: We use Firebase analytics tools to help us
+			measure traffic and usage trends for the Service. These tools collect
+			information sent by your device or our Service, including the WORDS you've
+			liked this assists us in improving the Service. We collect and use this
+			analytics information with analytics information from other Users so that
+			it cannot reasonably be used to identify any particular individual User.
+			Advertisements Information: Our free applications use advertising
+			currently provided by Admob and anonymous statistical information is
+			reported to them so they can see whether their adverts are effective. For
+			more information about Admob : http://www.admob.com/
+			<br />
+			<br />
+			2. HOW WE USE YOUR INFORMATION
+			<br />
+			<br />
+			In addition to some of the specific uses of information we describe in
+			this Privacy Policy, we may use information that we receive to: - help you
+			efficiently access your information after you sign in - remember
+			information so you will not have to re-enter it during your visit or the
+			next time you visit the Service; - provide, improve, test, and monitor the
+			effectiveness of our Service - monitor metrics such as total number of
+			visitors, traffic, and demographic patterns - diagnose or fix technology
+			problems
+			<br />
+			<br />
+			3. SHARING OF YOUR INFORMATION
+			<br />
+			<br />
+			We will not rent or sell your information to third parties outside Orphic
+			without your consent. What happens in the event of a change of control: If
+			we sell or otherwise transfer part or the whole of Orphic or our assets to
+			another organization (e.g., in the course of a transaction like a merger,
+			acquisition, bankruptcy, dissolution, liquidation), your information such
+			as name and email address, User Content and any other information
+			collected through the Service may be among the items sold or transferred.
+			You will continue to own your User Content. The buyer or transferee will
+			have to honor the commitments we have made in this Privacy Policy.
+			<br />
+			<br />
+			4. HOW WE STORE YOUR INFORMATION
+			<br />
+			<br />
+			Storage and Processing: Your information collected through the Service may
+			be stored and processed by the Firebase Servers. Firebase : Firebase is a
+			mobile and web application platform with tools and infrastructure designed
+			to help developers build high-quality apps. Firebase is made up of
+			complementary features that developers can mix-and-match to fit their
+			needs. Parent organization: Alphabet Inc. Headquarters: San Francisco More
+			Info : https://firebase.google.com/support/privacy/
+			<br />
+			<br />
+			5. YOUR CHOICES ABOUT YOUR INFORMATION
+			<br />
+			<br />
+			How long we keep your User Content: Following termination or deactivation
+			of your account, Orphic , or its Service Providers may not retain any
+			information (including your profile information). Learn more about
+			deleting your account.
+			<br />
+			<br />
+			6. HOW TO CONTACT US ABOUT A DECEASED USER
+			<br />
+			<br />
+			In the event of the death of an Orphic User, please contact us. We will
+			usually conduct our communication via email; should we require any other
+			information, we will contact you at the email address you have provided in
+			your request.
+			<br />
+			<br />
+			7. HOW TO CONTACT US If you have any questions about this Privacy Policy
+			or the Service, please email us at eclectik.devs@gmail.com.
+			<br />
+			<br />
+			8. CHANGES TO OUR PRIVACY POLICY Orphic may modify or update this Privacy
+			Policy from time to time, so please review it periodically. We may provide
+			you additional forms of notice of modifications or updates as appropriate
+			under the circumstances. Your continued use of Orphic or the Service after
+			any modification to this Privacy Policy will constitute your acceptance of
+			such modification.
+			<br />
+			<br />
+			9. ADVERTISEMENTS
+			<br />
+			<br />
+			Orphic uses Google's AdMob Ads service.
+		</div>
+	);
+}
+
+export default policy;
