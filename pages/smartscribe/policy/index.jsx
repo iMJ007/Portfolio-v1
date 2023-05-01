0d7fc0b48e68c4fee2a9f4062e49b53e5e85d4b7
iMJ007 <mohammed.junaid1000@gmail.com>
2023-05-01 17:25:29 +0530

    Added smartscribe policy

---
+++ pages/smartscribe/policy/index.jsx
@@ -0,0 +1,111 @@
+function policy() {
+	return (
+		<div style={{ color: 'white' }}>
+			Privacy Policy for SmartScribe Chrome Extension
+			<br />
+			<br />
+			<p>
+				SmartScribe ("Extension") is a Chrome extension that helps users
+				to generate text using the GPT from OpenAI API. This Privacy
+				Policy describes how we collect, use, and disclose information
+				from users of the Extension.
+			</p>
+			<br />
+			<ol>
+				<li>
+					<strong>Information We Collect</strong>
+					<br />
+					<br />
+					We may collect the following types of information from users
+					of the Extension:
+					<br />
+					<br />
+					<ul>
+						<li>
+							Email address: We collect users' email address to
+							verify their license.
+						</li>
+						<li>
+							OpenAI API key: We collect users' OpenAI API key to
+							authenticate on OpenAI API requests.
+						</li>
+						<li>
+							User input: We collect users' input to get processed
+							data response by GPT from OpenAI API. <em>
+                                <strong>
+                                                                We do not
+                                                                store any of the above information on our own
+                                                                servers.
+                                                            </strong>
+                            </em>
+						</li>
+					</ul>
+				</li>
+				<br />
+				<li>
+					<strong>Use of Information</strong>
+					<br />
+					<br />
+					We use the information we collect from users to:
+					<br />
+					<br />
+					<ul>
+						<li>Verify the license of the Extension user</li>
+						<li>Authenticate on OpenAI API requests</li>
+						<li>
+							Get processed data response by GPT from OpenAI API
+							We do not use the information for any other
+							purposes.
+						</li>
+					</ul>
+				</li>
+				<br />
+				<li>
+					<strong>Disclosure of Information</strong>
+					<br />
+					<br />
+					We do not disclose any of the information we collect to any
+					third parties.
+				</li>
+				<br />
+				<li>
+					<strong>Security</strong>
+					<br />
+					<br />
+					We take reasonable measures to protect the information we
+					collect from users of the Extension. However, please be
+					aware that no security measures are perfect or impenetrable,
+					and we cannot guarantee the security of your information.
+				</li>
+				<br />
+				<li>
+					<strong>Changes to this Privacy Policy</strong>
+					<br />
+					<br />
+					We may change this Privacy Policy from time to time. If we
+					make changes, we will notify you by revising the date at the
+					top of the policy and, in some cases, we may provide you
+					with additional notice (such as by adding a statement to our
+					website or by sending you a notification). We encourage you
+					to review the Privacy Policy whenever you use the Extension
+					to stay informed about our information practices and the
+					ways you can help protect your privacy.
+				</li>
+                <br />
+				<li>
+					<strong>Contact Us</strong>
+					<br />
+					<br />
+					If you have any questions or concerns about this Privacy
+					Policy or our practices with regard to the Extension, please
+					contact us at{' '}
+					<a href="mailto:contact@junaidgandhi.com">
+						contact@junaidgandhi.com
+					</a>.
+				</li>
+			</ol>
+		</div>
+	);
+}
+
+export default policy;
