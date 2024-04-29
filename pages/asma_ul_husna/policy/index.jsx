59a2c8dd447237abd7458b4c4d76c93102468815
Mohammed Junaid Gandhi <mohammed.junaid1000@gmail.com>
2024-04-29 16:47:31 +0530

    Added asma ul husna privacy policy

---
+++ pages/asma_ul_husna/policy/index.jsx
@@ -0,0 +1,65 @@
+function policy() {
+	return (
+		<div style={{ backgroundColor: "white" }}>
+			<h1>Privacy Policy for Asma-ul-Husna</h1>
+			<p>
+				<strong>Effective Date: 29 April 2024</strong>
+			</p>
+
+			<p>
+				Thank you for using Asma-ul-Husna! This Privacy Policy is
+				intended to help you understand what information, if any, is
+				collected when you use our mobile application and how that
+				information is used.
+			</p>
+
+			<h2>Information We Do Not Collect:</h2>
+			<p>
+				Asma-ul-Husna does not collect any personally identifiable
+				information from its users. We do not require you to provide any
+				personal information such as your name, email address, phone
+				number, or location.
+			</p>
+
+			<h2>Data Collection:</h2>
+			<p>
+				Our app does not collect any data from its users. We do not
+				track your usage of the app, your interactions with the app's
+				content, or any other information related to your usage.
+			</p>
+
+			<h2>Third-party Services:</h2>
+			<p>
+				Asma-ul-Husna does not integrate with any third-party services
+				or APIs that would collect data from users.
+			</p>
+
+			<h2>Children's Privacy:</h2>
+			<p>
+				Asma-ul-Husna is not directed at children under the age of 13,
+				and we do not knowingly collect any personal information from
+				children under 13. If you believe that we may have collected
+				personal information from a child under 13, please contact us
+				immediately so that we can take appropriate steps to remove such
+				information.
+			</p>
+
+			<h2>Changes to This Privacy Policy:</h2>
+			<p>
+				We reserve the right to update or change our Privacy Policy at
+				any time. Any changes will be posted to this page, and the
+				effective date will be updated accordingly. Your continued use
+				of Asma-ul-Husna after any such changes constitutes your
+				acceptance of the new Privacy Policy.
+			</p>
+
+			<h2>Contact Us:</h2>
+			<p>
+				If you have any questions or concerns about this Privacy Policy,
+				please contact us at eclectik.devs@gmail.com.
+			</p>
+		</div>
+	);
+}
+
+export default policy;
